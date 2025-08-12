3cf84007c47df9e01d0cd9c845f0851da7cad17a
anirudhs-1010 <anirudhs1010@gmail.com>
2025-08-12 16:19:22 -0700

    hash-object for saving object

--- anigit/data.py
+++ anigit/data.py
@@ -1,8 +1,15 @@
 import os
 
 
-GIT_DIR = '.ugit'
+GIT_DIR = '.anigit'
 
 
 def init ():
-    os.makedirs (GIT_DIR)
+    os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DR}/objects')
+
+def hash_object (data): #allows us to store objectes and opens this in the given directory
+    oid = hashlib.sha1(data).hexdigest() #currently using sha-1 hash 
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out: #writes it to our "object" database
+        out.write (data)
+    return oid
