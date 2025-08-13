8a7e4f002928149547433ba88b3b5e2121ed6644
anirudhs-1010 <anirudhs1010@gmail.com>
2025-08-12 17:35:10 -0700

    Finished hash-object command

--- anigit/cli.py
+++ anigit/cli.py
@@ -1,5 +1,7 @@
 import argparse
-
+import os
+import sys
+from . import data
 
 def main ():
     args = parse_args ()
@@ -7,16 +9,25 @@ def main ():
 
 
 def parse_args ():
+
     parser = argparse.ArgumentParser()
 
     commands = parser.add_subparsers(dest='command')
     commands.required = True
+    #All of this is content addressable storage
 
     init_parser = commands.add_parser('init')
     init_parser.set_defaults (func=init)
+
+    #hash object command code
     hash_object_parser = commands.add_parser ('hash-object')
     hash_object_parser.set_defaults (func=hash_object)
     hash_object_parser.add_argument ('file')
+
+    #cat file parser code
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
 
     return parser.parse_args ()
 
@@ -28,3 +39,8 @@ def init (args):
 def hash_object (args):
     with open (args.file, 'rb') as f:
         print (data.hash_object (f.read ()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
+
--- anigit/data.py
+++ anigit/data.py
@@ -1,15 +1,19 @@
 import os
-
+import hashlib
 
 GIT_DIR = '.anigit'
 
 
 def init ():
     os.makedirs(GIT_DIR)
-    os.makedirs(f'{GIT_DR}/objects')
+    os.makedirs(f'{GIT_DIR}/objects')
 
 def hash_object (data): #allows us to store objectes and opens this in the given directory
     oid = hashlib.sha1(data).hexdigest() #currently using sha-1 hash 
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out: #writes it to our "object" database
         out.write (data)
     return oid
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
