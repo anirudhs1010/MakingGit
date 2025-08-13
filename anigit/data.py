ba28d62c25dfb1400814912c11ceb77e5ca257fb
anirudhs-1010 <anirudhs1010@gmail.com>
2025-08-13 14:36:32 -0700

    Adding types to objects

--- anigit/data.py
+++ anigit/data.py
@@ -8,12 +8,17 @@ def init ():
     os.makedirs(GIT_DIR)
     os.makedirs(f'{GIT_DIR}/objects')
 
-def hash_object (data): #allows us to store objectes and opens this in the given directory
-    oid = hashlib.sha1(data).hexdigest() #currently using sha-1 hash 
+def hash_object (data, type = 'blob'): #allows us to store objectes and opens this in the given directory
+    obj = type_.encode () + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest() #currently using sha-1 hash but apparently the current Git implementation use SHA-256 due to hash collisions in SHA-1
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out: #writes it to our "object" database
-        out.write (data)
+        out.write(obj)
     return oid
 
-def get_object(oid):
+def get_object(oid, expected ='blob'):
     with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
-        return f.read()
+        obj = f.read()
+    type_, _, content = obj.partition(b'\x00') #
+    type_ = type_.decode()
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
